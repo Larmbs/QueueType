3d9cad666270f51c5ff71936bc89b93f4cfa4bbc
Liam Israel <138258207+Larmbs@users.noreply.github.com>
2024-06-11 17:30:04 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,6 @@ pub use queues::{merge_queues,        // Helper function to help manipulate queu
                 merge_sorted_queues,
                 queue_to_vec,
                 sorted_queue_to_vec,};  
-
+ 
 #[cfg(test)]
 mod tests; // Module containing tests for the queues
